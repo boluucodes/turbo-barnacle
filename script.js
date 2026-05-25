c202b57a071a9c0a56ea1167b8836f37f937e1fe
bxluwatif3 <sammiemarvie03@gmail.com>
2026-05-25 21:28:38 +0100

    test commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const colors = ['', 'grayish', 'pinky', 'whitish', 'greenish','orangy', 'yellowish', 'electricBlue', 'reddish']
+
+const sections = [...document.getElementsByTagName('section')]
+
+window.addEventListener('scroll', function () {
+
+  const scrollFromTop = window.pageYOffset
+
+  for (let i = 0; sections.length > i; i++) {
+
+    if (scrollFromTop <= sections[i].offsetTop) {
+      document.body.className = colors[i] 
+      break
+    } 
+
+  }
+
+})
